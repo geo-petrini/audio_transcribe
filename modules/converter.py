8bd0c078d3877ee5829f5348d3f951266e249c9b
geo-petrini <geo.petrini@gmail.com>
2025-02-11 23:01:29 +0100

    converter uses asyncio ffmpeg

--- modules/converter.py
+++ modules/converter.py
@@ -1,9 +1,44 @@
-import ffmpeg
+import asyncio
+from ffmpeg_asyncio import FFmpeg, Progress
 from flask import current_app
 
 def convert(inputfilename, outputfilename):
-    command = f' -loglevel error -i "{inputfilename}" -vn -ab 128k -ar 44100 -y -o "{outputfilename}"'
-    command.strip()
-    current_app.logger.debug(r'conversion command: {command}')
-    return ffmpeg.run_as_ffmpeg(command)
+    return asyncio.run(_run_ffmpeg(inputfilename, outputfilename))
         
+async def _run_ffmpeg(inputfilename, outputfilename):
+    errors = []
+    ffmpeg = (
+        FFmpeg()
+        .input(inputfilename)
+        .output(outputfilename)
+        .option(key='y')
+        .option(key='loglevel', value='warning')
+    )
+
+    @ffmpeg.on("start")
+    def on_start(arguments: list):
+        current_app.logger.debug(f'started with arguments: {arguments}')    
+
+    @ffmpeg.on("progress")
+    def on_progress(progress: Progress):
+        current_app.logger.debug(progress)
+
+    @ffmpeg.on("completed")
+    def on_completed():
+        current_app.logger.debug("Finished!")
+
+    @ffmpeg.on("terminated")
+    def on_exited(return_code: int):
+        current_app.logger.error(f"exited {return_code}")
+
+    @ffmpeg.on("stderr")
+    def on_error(line: str):
+        current_app.logger.error(f"conversion error: {line}")
+        errors.append(line)
+
+    await ffmpeg.execute()   #await pauses the execution of the function until it returns something, without blocking other async functions from running
+
+    if len(errors) > 0:
+        return 1
+    return ffmpeg._process.returncode   #this cannot be trusted, even with errors it returns 0
+
