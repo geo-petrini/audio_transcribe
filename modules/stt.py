861de092498c730c4507e919a7fe2926d1faeebd
geo-petrini <geo.petrini@gmail.com>
2025-02-18 22:50:27 +0100

    added logging when loading model

--- modules/stt.py
+++ modules/stt.py
@@ -2,7 +2,10 @@ import whisper
 from flask import current_app
 
 def init_model():
+    current_app.logger.info('loading speech to text model')
     current_app.config['stt_model'] = whisper.load_model("base")
+    current_app.logger.info('model loaded')    
+
 
 def getModel():
     if not 'stt_model' in current_app.config: init_model()
