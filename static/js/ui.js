048f94535b216290481df8858e84dc902c078f8c
geo-petrini <geo.petrini@gmail.com>
2025-03-19 10:24:36 +0100

    corrected region comments init

--- static/js/ui.js
+++ static/js/ui.js
@@ -761,8 +761,9 @@ class RegionsManager {
   }
 
   initRegionComments(region){
-    if (!region.data){ region.data = {comments:[]}}
+    if (!region.data){ region.data = {}}
     if (!'comments' in region.data) { region.data[comments] = []}
+    if ('comments' in region.data && region.data.comments == null) { region.data.comments = []}
   }
 
   saveComment(region){
